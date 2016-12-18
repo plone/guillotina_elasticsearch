d8887931bd81fadc380050fd1126eaf4574ba03e
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-12-18 19:51:19 +0100

    Adding initial travis check on elasticsearch

---
+++ pserver/elasticsearch/testing.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from plone.server.testing import PloneBaseLayer, TESTING_SETTINGS
+
+import unittest
+
+
+TESTING_SETTINGS['applications'] = ['pserver.elasticsearch']
+
+TESTING_SETTINGS['elasticsearch'] = {
+    "bulk_size": 50,
+    "index_name_prefix": "plone-",
+    "connection_settings": {
+        "endpoints": ["elastic:9200"],
+        "sniffer_timeout": 0.5
+    },
+    "index": {},
+    "mapping_overrides": {
+        "*": {
+        }
+    }
+}
+
+
+class ElasticSearchLayer(PloneBaseLayer):
+
+    def _get_site(self):
+        """
+        sometimes the site does not get updated data from zodb
+        this seems to make it
+        """
+        return self.layer.new_root()['plone']
+
+    @classmethod
+    def setUp(cls):
+        pass
+
+    @classmethod
+    def testSetUp(cls):
+        pass
+
+    @classmethod
+    def testTearDown(cls):
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        pass
+
+
+class ElasticSearchTestCase(unittest.TestCase):
+    ''' Adding the OAuth utility '''
+    layer = ElasticSearchLayer
