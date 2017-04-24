3c36552b09f1c0772435a4c65ed6aebd8aeb186c
vangheem <vangheem@gmail.com>
2017-04-24 07:47:49 -0500

    fix classifier

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGELOG.rst').read()),
     classifiers=[
-        'Framework :: Plone :: 7.0',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     url='https://pypi.python.org/pypi/guillotina_elasticsearch',
