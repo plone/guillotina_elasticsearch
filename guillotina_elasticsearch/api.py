d1bd767787a09298c4345b26833c187bf40590e9
Ramon Navarro Bosch <ramon@atlasense.com>
2017-05-11 15:20:13 +0200

    Forcing mapping to ES

--- guillotina_elasticsearch/api.py
+++ guillotina_elasticsearch/api.py
@@ -2,6 +2,8 @@ from guillotina import configure
 from guillotina.component import getUtility
 from guillotina.interfaces import ICatalogUtility
 from guillotina.interfaces import IContainer
+from guillotina.component import queryUtility
+from guillotina_elasticsearch.manager import get_mappings
 
 
 @configure.service(
@@ -10,3 +12,17 @@ from guillotina.interfaces import IContainer
 async def update_mapping(context, request):
     util = getUtility(ICatalogUtility)
     await util.migrate_index(context)
+
+
+@configure.service(
+    context=IContainer, name='@force_mapping', method='POST',
+    permission='plone.ManageCatalog')
+async def update_mapping(context, request):
+    catalog = queryUtility(ICatalogUtility)
+    index_name = await catalog.get_index_name(request.container)
+    mappings = get_mappings()
+    for key, value in mappings.items():
+        await catalog.conn.indices.put_mapping(index_name, key, value)
+    return {
+        'status': 'ok'
+    }
