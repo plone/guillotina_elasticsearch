49721f253c840df05d0b91355c5d7473b8ac32ee
vangheem <vangheem@gmail.com>
2018-05-23 20:43:04 -0400

    specify version reqs

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     include_package_data=True,
     packages=find_packages(exclude=['ez_setup']),
     install_requires=[
-        'guillotina>=3.1.6',
+        'guillotina>=3.1.6,<3.3.0',
         'aioes>=0.7.0',
         'ujson',
         'lru-dict'
