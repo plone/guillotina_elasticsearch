e9d3da96dccdb2c006f0550e0857845155a3b424
Jordi Masip <jordi@masip.cat>
2018-12-05 12:49:28 +0100

    More tests

--- guillotina_elasticsearch/tests/test_schema.py
+++ guillotina_elasticsearch/tests/test_schema.py
@@ -16,6 +16,22 @@ class IB(Interface):
     item = Float()
 
 
+class IC(Interface):
+    index_field('item', field_mapping={'type': 'float'})
+    item = Float()
+
+
 async def test_get_mappings_fails_on_conflict():
     with pytest.raises(Exception):
+        # Two content types define DIFFERENT mapping for same field ->
+        #   conflict!
         get_mappings(schemas=[IA, IB])
+
+
+async def test_get_mappings_nofails():
+    try:
+        # Two content types define SAME mapping for same field ->
+        #   everything is ok
+        get_mappings(schemas=[IB, IC])
+    except Exception:
+        pytest.fail("get_mappings() shouldn't fail if 'field_mapping' are the same")
