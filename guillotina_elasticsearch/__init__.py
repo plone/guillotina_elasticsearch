fc46e1b6fd24545c6901f6b687e4cadf1c61a654
Jordi Masip <jordi@masip.cat>
2021-03-05 16:56:01 +0100

    change

--- guillotina_elasticsearch/__init__.py
+++ guillotina_elasticsearch/__init__.py
@@ -5,10 +5,10 @@ from guillotina.component import get_utilities_for
 from guillotina.content import IResourceFactory
 from guillotina.utils import get_dotted_name
 
-import os
+# import os
 
 
-ELASTIC6 = os.environ.get("ES_VERSION", "").strip() == "6"
+ELASTIC6 = False  # os.environ.get("ES_VERSION", "").strip() == "6"
 
 
 def default_refresh():
