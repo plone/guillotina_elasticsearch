797c66ab72bf051f1df571f62fdce8a522d884b4
Jordi Masip <masipcat@users.noreply.github.com>
2021-04-30 12:20:42 +0200

    reverted changes

--- guillotina_elasticsearch/tests/test_custom_index.py
+++ guillotina_elasticsearch/tests/test_custom_index.py
@@ -7,7 +7,6 @@ from guillotina_elasticsearch.utils import get_content_sub_indexes
 from guillotina_elasticsearch.utils import get_installed_sub_indexes
 
 import aioelasticsearch
-import elasticsearch
 import json
 import pytest
 
@@ -227,11 +226,11 @@ async def test_delete_base_removes_index_from_elastic(es_requester):
 
         async def _test():
             # should find in content index but not main index
-            with pytest.raises(elasticsearch.exceptions.NotFoundError):
+            with pytest.raises(aioelasticsearch.exceptions.NotFoundError):
                 await catalog.get_connection().get(
                     index=content_index_name, doc_type="_all", id=resp["@uid"]
                 )
-            with pytest.raises(elasticsearch.exceptions.NotFoundError):
+            with pytest.raises(aioelasticsearch.exceptions.NotFoundError):
                 await catalog.get_connection().get(
                     index="guillotina-guillotina", doc_type="_all", id=cresp["@uid"]
                 )
