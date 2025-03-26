26774060dacd9b36d69c6652c0bc910a9170ad8e
nilbacardit26 <n.bacardit@iskra.cat>
2025-03-26 10:54:55 +0100

    passing tests

--- guillotina_elasticsearch/utility.py
+++ guillotina_elasticsearch/utility.py
@@ -9,6 +9,7 @@ from guillotina.component import get_utility
 from guillotina.event import notify
 from guillotina.exceptions import ContainerNotFound
 from guillotina.exceptions import RequestNotFound
+from guillotina.interfaces import IContainer
 from guillotina.interfaces import IFolder
 from guillotina.transactions import get_transaction
 from guillotina.utils import find_container
@@ -29,10 +30,9 @@ from guillotina_elasticsearch.interfaces import IIndexManager
 from guillotina_elasticsearch.parser import Parser
 from guillotina_elasticsearch.utils import format_hit
 from guillotina_elasticsearch.utils import get_migration_lock
+from guillotina_elasticsearch.utils import get_parent_by_interface
 from guillotina_elasticsearch.utils import noop_response
 from guillotina_elasticsearch.utils import safe_es_call
-from guillotina_elasticsearch.utils import get_parent_by_interface
-from guillotina.interfaces import IContainer
 
 import asyncio
 import backoff
